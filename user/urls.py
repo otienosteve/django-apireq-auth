231c7f4a543405a3468d587b57831e0aa2597465
otienosteve <steveotieno701@gmail.com>
2022-06-02 11:07:16 +0300

    initial commit

---
+++ user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import  views 
+
+urlpatterns=[
+path('',views.home,name='home'),
+path('login/',views.login_user),
+path('logout',views.logout_user),
+path('register/',views.register),
+path('api/',views.api)
+]
---
+++ user/views.py
@@ -0,0 +1,67 @@
+from http.client import HTTPResponse
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+# register
+# login
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+from urllib import request
+from django.contrib.auth import authenticate,login,logout
+from .forms import LoginForm 
+
+import json
+def home(request):
+
+    return render(request,'home.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+    
+def register(request):
+    form=UserCreationForm()
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponse(form)
+    context={'form':form}
+    return render(request,'register.html',context)
+
+def login_user(request):
+    form=LoginForm()
+    if request.method=='POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            usern=form.cleaned_data['username']
+            passw=form.cleaned_data['password']
+            user=authenticate(request,username=usern,password=passw)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                return HttpResponse('Such a user does not exist')
+        else:
+            return HttpResponse("Form is not Valid")
+    context={'form':form}
+    return render(request,'login.html',context)
+
+def api(req):
+    res=request.urlopen('https://www.fishwatch.gov/api/species')
+    print(res)
+    results=res.read()
+    print(type(results))
+    data=json.loads(results)
+    print(type(data))
+    
+    return render(req,'api.html',{'data':data[3]['Image Gallery']})
+
+#APIS is gjango
+#how to access and get data from an API
+# urllib/
+# djangorestframework
+# FastAPI
+
+#make a request to an API
+#get the data
+# display it in a template
